4dc87b44ab4c4d8103201bcbcfcbc111793b1e4f
ctofficial08 <77676642+ctofficial08@users.noreply.github.com>
2021-02-01 00:03:07 +0530

    Add files via upload
    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './style.css'
+import Login from './component/Login/Login';
+
+function App() {
+  return (
+    <div className="root-container">
+       <Login/>
+     </div>
+  );
+}
+
+export default App;
